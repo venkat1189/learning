ede24ac164d86a0e373777e66261b61f3aa42634
venkat <venkatakrishna.panga@gmail.com>
2019-12-18 18:13:06 +0530

    app name changing

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/app')
+@application.route('/app')
 def test():
     print('Enters into app')
     return "Hi There"
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
